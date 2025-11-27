29a05e0a168fc0edf285c38d81d0e2a6851c98ad
Rizat Sakmir <v.rapidtech.rizat@adira.co.id>
2025-11-27 16:53:19 +0700

    update migrations

--- src/migrations/20251112073406-create_default_categories_spend.js
+++ src/migrations/20251112073406-create_default_categories_spend.js
@@ -5,10 +5,10 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
       INSERT INTO categories_spend (id, account_id, name, status, created_dt) VALUES
-       ('019a7704-ec53-7d0a-a6cf-817b271cf3b9', 'all', 'monthly', true, NOW()),
-       ('019a7704-ec53-7652-b236-8ddcf65cc4cf', 'all', 'pocket', true, NOW()),
-       ('019a7704-ec53-705a-a696-4183b290fab1', 'all', 'internet', true, NOW()),
-       ('019a7704-ec53-767f-a32e-3428839e9415', 'all', 'garage', true, NOW())
+       ('019a7704-ec53-7d0a-a6cf-817b271cf3b9', 'all', 'food and drink', true, NOW()),
+       ('019a7704-ec53-7652-b236-8ddcf65cc4cf', 'all', 'internet', true, NOW()),
+       ('019a7704-ec53-705a-a696-4183b290fab1', 'all', 'service', true, NOW()),
+       ('019a7704-ec53-767f-a32e-3428839e9415', 'all', 'monthly', true, NOW())
       `);
   },
 
